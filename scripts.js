803c52d40fd4eb59d9683b9297380b2054ec8eca
Carolista <cjones.renaissancewoman@gmail.com>
2026-06-15 05:17:02 -0400

    removed solution

--- scripts.js
+++ scripts.js
@@ -2,45 +2,24 @@
 
 /** ONE ELEMENT TO BE MANIPULATED **/
 
-// Declare an object representing the box element
-let box = document.getElementById('box');
+// DEMO: Declare an object representing the box element
 
 /*** CHANGE THE BORDER THICKNESS ***/
 
-// Declare objects representing the three width buttons
-let four = document.getElementById('4');
-let eight = document.getElementById('8');
-let twelve = document.getElementById('12');
+// DEMO: Declare objects representing the three width buttons
 
-// Create a reusable function that manipulates the border-width property
-function changeBorderWidth(width) {
-    console.log(`Make it ${width} pixels thick!`);
-    box.style.borderWidth = width + 'px';
-}
+// DEMO: Create a reusable function that manipulates the border-width property
 
-// Create listeners to change thickness of box border
-four.addEventListener('click', () => changeBorderWidth(4));
-eight.addEventListener('click', () => changeBorderWidth(8));
-twelve.addEventListener('click', () => changeBorderWidth(12));
+// DEMO: Create listeners to change thickness of box border
 
 /*** CHANGE THE BORDER COLOR ***/
 
-// Declare objects representing the three color buttons
-let red = document.getElementById('red');
-let yellow = document.getElementById('yellow');
-let blue = document.getElementById('blue');
+// DEMO: Declare objects representing the three color buttons
 
-// Create a reusable function to manipulate the border-color property
-function changeBorderColor(name, hexCode) {
-	console.log(`Make it ${name}!`);
-	box.style.borderColor = hexCode;
-}
+// DEMO: Create a reusable function to manipulate the border-color property
 
-// Create listeners to change color of border to same hex values as buttons
-// Note: DOM can't access style sheets, so we must provide the color code
-red.addEventListener('click', () => changeBorderColor('red', '#a7240d'));
-yellow.addEventListener('click', () => changeBorderColor('yellow', '#ddac0a'));
-blue.addEventListener('click', () => changeBorderColor('blue', '#0d64b6'));
+// DEMO: Create listeners to change color of border to same hex values as buttons
+// Note: DOM can't access style sheets, so we must provide the color codes
 
 /*** CHANGE THE BORDER STYLE ***/
 
